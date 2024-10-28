3be13b5689d98224d7b06f989199839ac040ec56
Jarakei43 <134204371+Jarakei43@users.noreply.github.com>
2024-10-28 18:25:56 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+// N1
+// const num =[1, 3, 7, 8, 10]
+// const findNum = num.find((item) => {
+//     return item%2 == 0
+// })
+// console.log(findNum);
+
+//N2
+// const animals = ["кот", "собака", "тигр", "слон"];
+// const findAnimals = animals.find((item) => {
+//   return item.length > 5;
+// });
+// console.log(findAnimals);
+
+//N3
+// const bigger = [5, 9, 12, 3, 18]
+// const findBigger = bigger.find((item => {
+//     return item > 10
+// }))
+// console.log(findBigger);
+
+//N4
+// const users = [
+//   { name: "Алекс", age: 25 },
+//   { name: "Мария", age: 17 },
+//   { name: "Иван", age: 19 },
+// ];
+// const findUsers = users.find((item) => {
+//     return item.name == "Мария"
+// });
+// console.log(findUsers);
+
+// N5
+// const less = [3, 5, -2, -8, 7]
+// const findLess = less.find((item => {
+//     return item < 0
+// }))
+// console.log(findLess);
